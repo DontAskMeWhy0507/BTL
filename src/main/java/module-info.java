3d7a4240c06bf7b7fc3055c8800d46ae970e109c
Tuan Trung <62925438+VuTienTuanTrung@users.noreply.github.com>
2024-10-17 10:50:52 +0700

    Them vao

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module org.example.demo6 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+    requires annotations;
+    requires java.sql;
+
+    opens org.example.demo6 to javafx.fxml;
+    exports org.example.demo6;
+}

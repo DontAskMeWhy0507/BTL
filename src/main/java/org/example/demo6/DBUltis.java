d07586521f8e49ef68adda8dac2b7f0bdc9b0761
Vu Thanh Tung <160875999+TR01b17B0y5@users.noreply.github.com>
2024-10-17 11:13:53 +0700

    abcs

---
+++ src/main/java/org/example/demo6/DBUltis.java
@@ -0,0 +1,171 @@
+package org.example.demo6;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+import org.controlsfx.control.tableview2.filter.filtereditor.SouthFilter;
+
+import java.io.IOException;
+import java.sql.*;
+
+public class DBUltis {
+    public static void changescene(ActionEvent event, String fxmlFile, String title) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(HelloApplication.class.getResource(fxmlFile));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle(title);
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public static void signUp(ActionEvent event,String id, String username, String password) {
+        Connection connection = null;
+        PreparedStatement psInsert = null;
+        PreparedStatement psCheckUserExist = null;
+        PreparedStatement psCheckIDExist = null;
+        ResultSet rsUsername = null;
+        ResultSet rsID = null;
+
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:src//Library");
+
+            psCheckUserExist = connection.prepareStatement("SELECT * FROM USERS WHERE username = ?");
+            psCheckUserExist.setString(1, username);
+            rsUsername = psCheckUserExist.executeQuery();
+
+            psCheckIDExist = connection.prepareStatement("SELECT * FROM USERS WHERE ID = ?");
+            psCheckIDExist.setString(1, id);
+            rsID = psCheckIDExist.executeQuery();
+
+            if (rsUsername.isBeforeFirst()) {
+                System.out.println("User already exists");
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText("You cannot use this username");
+                alert.show();
+            } else if (rsID.isBeforeFirst()) {
+                System.out.println("ID already exists");
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText("You cannot use this ID");
+                alert.show();
+            }else {
+                psInsert = connection.prepareStatement("INSERT INTO USERS (ID, USERNAME, PASSWORD) VALUES (?, ?, ?)");
+                psInsert.setString(1, id);
+                psInsert.setString(2, username);
+                psInsert.setString(3, password);
+                psInsert.executeUpdate();
+                System.out.println("User created");
+                changescene(event, "/View/Login.fxml", "Login!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (rsID != null) {
+                try {
+                    rsUsername.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (rsUsername != null) {
+                try {
+                    rsUsername.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (psCheckIDExist != null) {
+                try {
+                    psCheckIDExist.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (psCheckUserExist != null) {
+                try {
+                    psCheckUserExist.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (psInsert != null) {
+                try {
+                    psInsert.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    public static void logIn(ActionEvent event, String username, String password) {
+        Connection connection = null;
+        PreparedStatement psCheckUserExist = null;
+        ResultSet rs = null;
+
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:src//Library");
+
+            psCheckUserExist = connection.prepareStatement("SELECT * FROM USERS WHERE username = ? AND password = ?");
+            psCheckUserExist.setString(1, username);
+            psCheckUserExist.setString(2, password);
+            rs = psCheckUserExist.executeQuery();
+
+            if (!rs.isBeforeFirst()) {
+                System.out.println("User does not exist");
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText("Your username or password is incorrect");
+                alert.show();
+            } else {
+                while (rs.next()) {
+                    String retrievedPassword = rs.getString("password");
+                    if (retrievedPassword.equals(password)) {
+                        changescene(event, "/View/Home.fxml", "Home to Library");
+                    } else {
+                        System.out.println("Password is incorrect");
+                        Alert alert = new Alert(Alert.AlertType.ERROR);
+                        alert.setContentText("Your username or password is incorrect");
+                        alert.show();
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (rs != null) {
+                try {
+                     rs.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (psCheckUserExist != null) {
+                try {
+                    psCheckUserExist.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}

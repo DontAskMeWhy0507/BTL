390e2fd34fcb4f016327ec9e3cad8114e135a362
Vu Thanh Tung <160875999+TR01b17B0y5@users.noreply.github.com>
2024-10-17 13:54:03 +0700

    sua

--- src/main/java/org/example/demo6/DBUltis.java
+++ src/main/java/org/example/demo6/DBUltis.java
@@ -14,7 +14,7 @@ import java.sql.*;
 
 public class DBUltis {
     public static void changescene(ActionEvent event, String fxmlFile, String title) {
-        Parent root = null;
+        Parent root;
         try {
             root = FXMLLoader.load(HelloApplication.class.getResource(fxmlFile));
         } catch (IOException e) {

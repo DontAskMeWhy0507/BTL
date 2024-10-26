eb4fb12a7feb3e5777b03afcc3eeb62b9cc0772d
Vu Thanh Tung <160875999+TR01b17B0y5@users.noreply.github.com>
2024-10-26 09:54:06 +0700

    chodng nhap

--- src/main/java/org/example/demo6/Controller/LoginController.java
+++ src/main/java/org/example/demo6/Controller/LoginController.java
@@ -1,13 +1,16 @@
 package org.example.demo6.Controller;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import org.example.demo6.DBUltis;
 
 
@@ -28,17 +31,24 @@ public class LoginController {
     @FXML
     private JFXButton buttonForgotPassword;
 
+
     public void loginToHome(ActionEvent event) {
+        // Tạo và hiển thị cảnh báo
+
+
         try {
+            // Giả lập độ trễ khi đăng nhập (thay bằng logic thực tế)
             DBUltis.logIn(event, tf_username.getText(), tf_password.getText());
-        } catch (Exception e) {
-            e.printStackTrace();
-            Throwable cause = e.getCause();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            Throwable cause = ex.getCause();
             if (cause != null) {
                 cause.printStackTrace();
             }
         }
     }
+
+
 
     public void signUp1(ActionEvent event) {
         try {

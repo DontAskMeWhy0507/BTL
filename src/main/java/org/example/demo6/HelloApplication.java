4d7f275f4e97057a35f01dd72aea54ea93d9770d
Dang Anh Que <160758822+cinnamoll@users.noreply.github.com>
2024-11-06 22:56:56 +0700

    tam thoi la login

--- src/main/java/org/example/demo6/HelloApplication.java
+++ src/main/java/org/example/demo6/HelloApplication.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.text.Font;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
@@ -12,6 +13,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(@NotNull Stage stage) throws IOException {
+        Font font = Font.loadFont(getClass().getResourceAsStream("/Font/Merriweather/Merriweather-Regular.ttf"), 20);
+        Font font1 = Font.loadFont(getClass().getResourceAsStream("/Font/DancingScript/DancingScript-SemiBold.ttf"), 20);
         Parent root = FXMLLoader.load(HelloApplication.class.getResource("/View/LoginScene/Login.fxml"));
         Scene scene = new Scene(root);
         stage.setTitle("Library management system");

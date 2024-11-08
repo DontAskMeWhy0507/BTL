509854d3b65af5d371a7a96b659b92f057ac315d
trung <tuantrung0507@gmail.com>
2024-11-08 19:42:31 +0700

    Book Preview

--- src/main/java/org/example/demo6/Controller/Scene/BookPreviewController.java
+++ src/main/java/org/example/demo6/Controller/Scene/BookPreviewController.java
@@ -8,6 +8,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
@@ -25,8 +26,6 @@ import java.io.IOException;
 public class BookPreviewController {
 
     private Book currentBook;
-
-
     @FXML
     private Label bookTitleLabel;
 
@@ -37,7 +36,13 @@ public class BookPreviewController {
     private Text bookPublishedDate;
 
     @FXML
-    private Label bookCategoryLabel;
+    private Text bookCategoryLabel;
+
+    @FXML
+    private Text bookPublisher;
+
+    @FXML
+    private Text bookLanguage;
 
     @FXML
     private Text bookDescription;
@@ -45,12 +50,23 @@ public class BookPreviewController {
     @FXML
     private ImageView bookCoverImage;
 
+
+    public void postComment() {
+        // Add the comment to the current book
+        // currentBook.addComment(commentTextArea.getText());
+        // Update the book in the database
+        // Library.updateBook(currentBook);
+    }
+
     public void setBookData(Book book) {
         currentBook = book;
         bookTitleLabel.setText(book.getTitle());
         bookDescription.setText(book.getDescription());
         bookAuthor.setText(book.getAuthor());
         bookPublishedDate.setText(book.getPublishedDate());
+        bookCategoryLabel.setText(book.getCategory());
+        bookPublisher.setText(book.getPublisher());
+        bookLanguage.setText(book.getLanguage());
 
         // Check if cover image path is available and valid
         if (book.getCoverImagePath() != null && !book.getCoverImagePath().isEmpty()) {

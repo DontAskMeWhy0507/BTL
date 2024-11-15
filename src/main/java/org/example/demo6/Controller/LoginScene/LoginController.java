9830e52f263044612401fba0ec3cdaaa665c4bc0
Dang Anh Que <160758822+cinnamoll@users.noreply.github.com>
2024-11-15 12:47:55 +0700

    login with transition

--- src/main/java/org/example/demo6/Controller/LoginScene/LoginController.java
+++ src/main/java/org/example/demo6/Controller/LoginScene/LoginController.java
@@ -3,18 +3,28 @@ package org.example.demo6.Controller.LoginScene;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXRippler;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -25,14 +35,17 @@ import org.example.demo6.Classes.Library;
 import org.example.demo6.Classes.Music;
 import javafx.scene.input.KeyCode;
 
+import java.io.IOException;
 import java.net.URL;
 import java.security.Key;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 import static org.example.demo6.Controller.GeneralController.changescene;
 
-
 public class LoginController implements Initializable {
+    @FXML
+    private StackPane switchScene;
 
     @FXML
     private AnchorPane mainPane;
@@ -50,7 +63,13 @@ public class LoginController implements Initializable {
     private PasswordField tf_password;
 
     @FXML
+    private TextField showPassword;
+
+    @FXML
     private JFXCheckBox remember;
+
+    @FXML
+    private JFXCheckBox showPass;
 
     @FXML
     private JFXButton buttonForgotPassword;
@@ -61,7 +80,71 @@ public class LoginController implements Initializable {
     @FXML
     private JFXRippler loginRippler;
 
+    @FXML
+    void getPassword(ActionEvent event) {
+        if (showPass.isSelected()) {
+            showPassword.setText(tf_password.getText());
+            tf_password.setVisible(false);
+            showPassword.setVisible(true);
+        } else {
+            showPassword.setText(tf_password.getText());
+            tf_password.setText(showPassword.getText());
+            tf_password.setVisible(true);
+            showPassword.setVisible(false);
+        }
+    }
 
+    @FXML
+    private void loadSignUp(ActionEvent event) throws IOException {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/View/LoginScene/SignUp.fxml"));
+            Scene scene = buttonSignUp.getScene();
+
+            root.translateYProperty().set(scene.getHeight());
+            switchScene.getChildren().add(root);
+
+            Timeline timeline = new Timeline();
+            KeyValue kv = new KeyValue(root.translateYProperty(), 0, Interpolator.EASE_IN);
+            KeyFrame kf = new KeyFrame(Duration.seconds(1), kv);
+            timeline.getKeyFrames().add(kf);
+            timeline.setOnFinished(event1 ->{
+                switchScene.getChildren().remove(mainPane);
+            });
+            timeline.play();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Throwable cause = e.getCause();
+            if (cause != null) {
+                cause.printStackTrace();
+            }
+        }
+    }
+
+    @FXML
+    private void loadForgotPassword(ActionEvent event) throws IOException {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/View/LoginScene/ForgotPassword.fxml"));
+            Scene scene = buttonForgotPassword.getScene();
+
+            root.translateYProperty().set(-scene.getHeight());
+            switchScene.getChildren().add(root);
+
+            Timeline timeline = new Timeline();
+            KeyValue kv = new KeyValue(root.translateYProperty(), 0, Interpolator.EASE_IN);
+            KeyFrame kf = new KeyFrame(Duration.seconds(1), kv);
+            timeline.getKeyFrames().add(kf);
+            timeline.setOnFinished(event1 ->{
+                switchScene.getChildren().remove(mainPane);
+            });
+            timeline.play();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Throwable cause = e.getCause();
+            if (cause != null) {
+                cause.printStackTrace();
+            }
+        }
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -85,30 +168,6 @@ public class LoginController implements Initializable {
         } catch (Exception ex) {
             ex.printStackTrace();
             Throwable cause = ex.getCause();
-            if (cause != null) {
-                cause.printStackTrace();
-            }
-        }
-    }
-
-    public void signUp1(ActionEvent event) {
-        try {
-            changescene(event, "/View/LoginScene/SignUp.fxml", "Sign Up!");
-        } catch (Exception e) {
-            e.printStackTrace();
-            Throwable cause = e.getCause();
-            if (cause != null) {
-                cause.printStackTrace();
-            }
-        }
-    }
-
-    public void switchForgotPassword(ActionEvent event) {
-        try {
-            changescene(event, "/View/LoginScene/ForgotPassword.fxml", "ForgotPassword!");
-        } catch (Exception e) {
-            e.printStackTrace();
-            Throwable cause = e.getCause();
             if (cause != null) {
                 cause.printStackTrace();
             }

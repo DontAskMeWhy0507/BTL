652bb5010e198c7429ab4be73b4e89ff525043ec
trung <tuantrung0507@gmail.com>
2024-11-18 17:51:47 +0700

    loi loi loi

--- src/main/java/org/example/demo6/PathCheck.java
+++ src/main/java/org/example/demo6/PathCheck.java
@@ -12,23 +12,6 @@ import java.util.List;
 
 public class PathCheck {
     public static void main(String[] args) {
-        String url = "jdbc:sqlite:database//LibraryMain"; // Đường dẫn đến file SQLite của bạn
-        String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
-        String username = "admin";
-        String password = "123";
-
-        User user = null;
-        try (Connection conn = DriverManager.getConnection(url);
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setString(1, username);
-            pstmt.setString(2, password);
-            ResultSet rs = pstmt.executeQuery();
-            if (rs.next()) {
-                LocalDate dateOfBirth = LocalDate.parse(rs.getString("DATE_OF_BIRTH"));
-                System.out.println(dateOfBirth.getDayOfMonth());
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+        System.out.println(LocalDate.now());
     }
 }

aed1cdee60cb4b3b944075cc9ba44f3c94084896
Tuan Trung <tuantrung0705@gmail.com>
2024-11-19 12:23:32 +0700

    fix add user

--- src/main/java/org/example/demo6/Controller/AdminScene/Page/AddUser.java
+++ src/main/java/org/example/demo6/Controller/AdminScene/Page/AddUser.java
@@ -3,13 +3,16 @@ package org.example.demo6.Controller.AdminScene.Page;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import org.example.demo6.Classes.DBUltis;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.time.LocalDate;
 
 public class AddUser {
+    DBUltis dbUltis = new DBUltis();
 
     @FXML
     private ChoiceBox<String> choiceRole;
@@ -47,37 +50,39 @@ public class AddUser {
             alert.setHeaderText(null);
             alert.setContentText("Please fill all fields");
             alert.showAndWait();
-        } else {
-            // Add user to database
-            String url = "jdbc:sqlite:database//LibraryMain";
-            String checkQuery = "SELECT COUNT(*) FROM USERS WHERE id = ?";
-            String insertQuery = "INSERT INTO USERS (id, username, password, role, email, date, avatar) VALUES (?, ?, ?, ?, ?, ?, ?)";
 
-            try (Connection conn = DriverManager.getConnection(url);
-                 PreparedStatement checkStmt = conn.prepareStatement(checkQuery);
-                 PreparedStatement insertStmt = conn.prepareStatement(insertQuery)) {
+        } else if (dbUltis.findQuery("SELECT * FROM users WHERE id = '" + tf_id.getText() + "'")) {
+            // Alert user that id is already taken
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning");
+            alert.setHeaderText(null);
+            alert.setContentText("ID is already taken");
+            alert.showAndWait();
 
-                checkStmt.setInt(1, Integer.parseInt(tf_id.getText()));
-                ResultSet rs = checkStmt.executeQuery();
-                if (rs.next() && rs.getInt(1) > 0) {
-                    // ID already exists
-                    sucessLabel.setText("Thất bại");
-                } else {
-                    // Add user to database
-                    insertStmt.setInt(1, Integer.parseInt(tf_id.getText()));
-                    insertStmt.setString(2, tf_username.getText());
-                    insertStmt.setString(3, tf_password.getText());
-                    insertStmt.setString(4, choiceRole.getValue());
-                    insertStmt.setString(5, tf_email.getText());
-                    insertStmt.setString(6, dateOfBirth.getValue().toString());
-                    insertStmt.setString(7, ""); // Assuming avatar is not provided
+        } else if (dbUltis.findQuery("SELECT * FROM users WHERE username = '" + tf_username.getText() + "'")) {
+            // Alert user that username is already taken
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning");
+            alert.setHeaderText(null);
+            alert.setContentText("Username is already taken");
+            alert.showAndWait();
 
-                    insertStmt.executeUpdate();
-                    sucessLabel.setText("Thành công");
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        } else if (dbUltis.findQuery("SELECT * FROM users WHERE email = '" + tf_email.getText() + "'")) {
+            // Alert user that email is already taken
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning");
+            alert.setHeaderText(null);
+            alert.setContentText("Email is already taken");
+            alert.showAndWait();
+
+        }
+        else {
+            // Insert user into database
+            dbUltis.loadQuery("INSERT INTO users (username, password, role, email, avatar, last_access, streak, date_of_birth, longest_streak) " +
+                    "VALUES ('" + tf_username.getText() + "', '" + tf_password.getText() + "', '" + choiceRole.getValue() + "', '" +
+                    tf_email.getText() + "', '" + "/Image/Avatar/Gekko.png" + "', '" + LocalDate.now().toString() + "', 0, '" +
+                    dateOfBirth.getValue().toString() + "', 0)");
+            sucessLabel.setText("User added successfully");
         }
     }
 }

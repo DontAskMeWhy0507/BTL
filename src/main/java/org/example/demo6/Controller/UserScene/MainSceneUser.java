8f424bffa17136278636b761c3faadbed3751eaf
Vu Thanh Tung <160875999+TR01b17B0y5@users.noreply.github.com>
2024-11-19 22:05:33 +0700

    thêm streak

--- src/main/java/org/example/demo6/Controller/UserScene/MainSceneUser.java
+++ src/main/java/org/example/demo6/Controller/UserScene/MainSceneUser.java
@@ -185,4 +185,18 @@ public class MainSceneUser {
     }
 
 
+    public void changeToStreak() {
+        try {
+            FXMLLoader loader1 = new FXMLLoader(getClass().getResource("/View/AdminScene/Page/Streak.fxml"));
+            Parent StreakView = loader1.load();
+
+            // Đặt nội dung mới vào ScrollPane
+            mainScrollPane.setContent(StreakView);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }

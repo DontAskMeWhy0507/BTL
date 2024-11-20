8ce7cf8a92cc19b9fdf3f02b3d7c34d1e426bdf5
trung <tuantrung0507@gmail.com>
2024-11-20 21:46:07 +0700

    Them api AI chat bot

--- src/main/java/org/example/demo6/PathCheck.java
+++ src/main/java/org/example/demo6/PathCheck.java
@@ -1,17 +1,32 @@
+
 package org.example.demo6;
+import org.example.demo6.Classes.ApiGoogleGemini;
+import org.example.demo6.Classes.Book;
+import org.example.demo6.Classes.apiGoogleBooks;
 
-import org.example.demo6.Classes.*;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.sql.*;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.List;
 
 public class PathCheck {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) throws IOException {
+        apiGoogleBooks apiGoogleBooks = new apiGoogleBooks();
+        List<Book> books = apiGoogleBooks.searchBooks1("Harry");
+        for (Book book : books) {
+            System.out.println(book.getIsbn());
+            System.out.println(book.getTitle());
+            System.out.println(book.getAuthor());
+            System.out.println(book.getPublisher());
+            System.out.println(book.getPublishedDate());
+            System.out.println(book.getDescription());
+            System.out.println(book.getCoverImagePath());
+            System.out.println(book.getCategory());
+        }
     }
+
+
 }
